7d4c8a055d2a88088a9e070a56daac806257eb25
ShubhamGaikwad1748 <106225975+ShubhamGaikwad1748@users.noreply.github.com>
2022-05-25 12:06:54 +0530

    This is sample

--- lib/src/main/java/com/cg/eaction/controller/SampleController.java
+++ lib/src/main/java/com/cg/eaction/controller/SampleController.java
@@ -3,8 +3,7 @@ package com.cg.eaction.controller;
 public class SampleController {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Java ");
 	}
 
 }
